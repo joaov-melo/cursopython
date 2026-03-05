6d4652054829fc66f8ec85bf332a6ab21d8fb70c
joaov-melo <joaovmaf@gmail.com>
2026-03-05 18:28:07 -0300

    upload de arquvios

---
+++ while_break.py
@@ -0,0 +1,25 @@
+'''
+Repetições
+While(enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+'''
+
+# condição = True
+# while condição:
+#     nome = input('Qual seu nome: ')
+#     if nome == 'sair':
+#         break
+#     print(f'Seu nome é {nome}')
+
+
+# print('acabou')
+
+contador = 0
+
+while contador<10:
+    print(contador)
+    contador = contador + 1
+    
+
+print('Acabou')
