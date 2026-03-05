87e0833c773cb7ba69a9d3cad4a5e7b36d4d36ac
joaov-melo <joaovmaf@gmail.com>
2026-03-05 18:04:46 -0300

    upload de arquivos

---
+++ built_in.py
@@ -0,0 +1,8 @@
+'''
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vi: str, int, float, bool 
+'''
+string = 'João Vitor' 
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(outra_variavel)
+print(string.zfill(15))
